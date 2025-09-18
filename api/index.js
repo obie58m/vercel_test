f9315660efcdf2d04e4e239ddae9d2182f7b903e
hades <sn102a1k46@gmail.com>
2025-09-18 23:08:01 +0900

    update-8

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 
+app.use(cors({ origin: "*" }));
 // --- Mongo connection (reused across invocations) ---
 let clientPromise;  // a single promise reused by all requests
 let db;
